b21ce765b9b64aba5ed9d2e6487713d03fb241e5
abhave-cc <ameya.bhave@computacenter.com>
2026-06-10 15:06:35 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -191,6 +191,7 @@ runButton.addEventListener("click", async () => {
     setRagContext(body.rag_context);
 
     setHackerNews(body.hacker_news);
+    
     setArxivPapers(body.arxiv_papers);
 
     resultsEl.classList.remove("hidden");
